30099392009ea87fc080c35ca23e4614f058379c
Milaan Dahiya <milaandahiya@gmail.com>
2023-06-19 15:29:46 -0500

    cleanup

--- Scripts/testingScript.py
+++ Scripts/testingScript.py
@@ -7,10 +7,7 @@ import numpy as np
 import os
 import json
 from PIL import Image
-from omni.isaac.core import World
-from omni.isaac.core.objects import DynamicCuboid
 import omni.replicator.core as rep
-from omni.isaac.core.utils.semantics import add_update_semantics
 
 
 context = omni.usd.get_context()
@@ -19,24 +16,22 @@ context.open_stage('/home/gjfh119/Documents/USD Files/alien.usd') # Open saved d
 print("Opened Stage")
 
 with rep.new_layer():
-    # Create cameras (corners of room are -500 to 500 on the x and -250 to 250 on the y)
-    camera1 = rep.create.camera(position=(-4900, -2400, 12500), rotation=(90, 0, -53), focus_distance=(100), f_stop=5.2)
-    camera2 = rep.create.camera(position=(0, 100, 12500), rotation=(90, 0, 0), focus_distance=(100), f_stop=5.2)
+    # Create cameras (corners of room are -10 to 10 on the x and -5 to 5 on the y/z)
+    camera1 = rep.create.camera(position=(-99, 0, 250), rotation=(90, 0, -90), focal_length=20.0, focus_distance=1000, f_stop=2.0)
+    camera2 = rep.create.camera(position=(99, 0, 250), rotation=(90, 0, 90), focal_length=20.0, focus_distance=1000, f_stop=2.0)
     # ...
     print("Created Cameras")
 
-    # Render frames at 720p
-    render_product1 = rep.create.render_product(camera1, (1280, 720))
-    render_product2 = rep.create.render_product(camera2, (1280, 720))
-    print("Rendered Frames")
+# Render frames at 720p
+render_product1 = rep.create.render_product(camera1, (1280, 720))
+render_product2 = rep.create.render_product(camera2, (1280, 720))
+print("Rendered Frames")
 
-    # Write to file
-    writer = rep.WriterRegistry.get("BasicWriter")
-    writer.initialize(output_dir=f"~/home/gjfh119/Documents/DataOut/", rgb=True, distance_to_camera=True)
-    writer.attach([render_product1, render_product2])
-    print("Wrote to file")
+# Write to file
+writer = rep.WriterRegistry.get("BasicWriter")
+writer.initialize(output_dir=f"~/home/gjfh119/Documents/DataOut/", rgb=True, distance_to_camera=True)
+writer.attach([render_product1, render_product2])
+print("Wrote to file")
 
-    # Preview?
-    rep.orchestrator.preview()
-
-    # TODO rescale the creation it's like 10x or 100x too big because the camera physics are messed up
+# Preview?
+rep.orchestrator.run()
