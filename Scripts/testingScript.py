d0558d0c2450d96246ecccff6d4828dc241b3c9d
Milaan Dahiya <milaandahiya@gmail.com>
2023-06-19 16:33:09 -0500

    getting data out

--- Scripts/testingScript.py
+++ Scripts/testingScript.py
@@ -9,14 +9,13 @@ import omni.replicator.core as rep
 
 context = omni.usd.get_context()
 # stage = context.get_stage() # Get the current stage in Omniverse
-# context.open_stage('/home/gjfh119/Documents/USD Files/alien.usd') # Open saved dancing alien stage
+context.open_stage('/home/gjfh119/Documents/USD Files/alien.usd') # Open saved dancing alien stage
 print("Opened Stage")
 
-with rep.new_layer():
-    # Create cameras (corners of room are -10 to 10 on the x and -5 to 5 on the y/z)
-    camera1 = rep.create.camera(position=(-99, 0, 250), rotation=(90, 0, -90), focal_length=20.0, focus_distance=1000, f_stop=2.0)
-    camera2 = rep.create.camera(position=(99, 0, 250), rotation=(90, 0, 90), focal_length=20.0, focus_distance=1000, f_stop=2.0)
-    # ...
+# Create cameras (corners of room are -10 to 10 on the x and -5 to 5 on the y/z)
+camera1 = rep.create.camera(position=(-990, 0, 250), rotation=(0, 0, -180), focal_length=20.0, focus_distance=1000, f_stop=2.0)
+camera2 = rep.create.camera(position=(990, 0, 250), rotation=(0, 0, 0), focal_length=20.0, focus_distance=1000, f_stop=2.0)
+# ...
 print("Created Cameras")
 
 # Render frames at 720p
@@ -26,7 +25,7 @@ print("Rendered Frames")
 
 # Write to file
 writer = rep.WriterRegistry.get("BasicWriter")
-writer.initialize(output_dir=f"~/home/gjfh119/Documents/DataOut/", rgb=True, distance_to_camera=True)
+writer.initialize(output_dir="/home/gjfh119/Documents/DataOut/", rgb=True, distance_to_camera=True)
 writer.attach([render_product1, render_product2])
 print("Wrote to file")
 
