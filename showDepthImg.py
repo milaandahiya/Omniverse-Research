b62ea2a9e5e6eb19246c1557369b7e923dd16710
Milaan Dahiya <milaandahiya@gmail.com>
2023-06-20 16:17:32 -0500

    open3d rgbd image (no PIL)

--- showDepthImg.py
+++ showDepthImg.py
@@ -1,6 +1,18 @@
-from PIL import Image
+import open3d as o3d
 import numpy as np
+import matplotlib.pyplot as plt
 
-img = np.load("/home/gjfh119/Documents/DataOut/RenderProduct_Replicator/distance_to_camera/distance_to_camera_0000.npy")
-pil_img = Image.fromarray(img)
-pil_img.show()
+
+# Get color image
+color_img = o3d.io.read_image("/home/gjfh119/Documents/DataOut/RenderProduct_Replicator/rgb/rgb_0000.png")
+
+# Load numpy array with depth values and convert to depth image
+depth_array = np.load("/home/gjfh119/Documents/DataOut/RenderProduct_Replicator/distance_to_camera/distance_to_camera_0000.npy")
+print(depth_array.shape)
+depth_img = o3d.geometry.Image(depth_array)
+
+#Create RGBD image from color and depth images
+rgbd_img = o3d.geometry.RGBDImage.create_from_color_and_depth(color_img, depth_img, convert_rgb_to_intensity=False)
+
+plt.imshow(rgbd_img.depth)
+plt.show()
