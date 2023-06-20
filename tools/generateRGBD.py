f925abc6affb013975cc74574e3b9216021883ca
Milaan Dahiya <milaandahiya@gmail.com>
2023-06-20 16:36:38 -0500

    modularize

--- tools/generateRGBD.py
+++ tools/generateRGBD.py
@@ -2,20 +2,23 @@ import open3d as o3d
 import numpy as np
 import matplotlib.pyplot as plt
 
+def generateRGBD(display: bool):
+    # Get color image
+    color_img = o3d.io.read_image("/home/gjfh119/Documents/DataOut/RenderProduct_Replicator/rgb/rgb_0000.png")
 
-# Get color image
-color_img = o3d.io.read_image("/home/gjfh119/Documents/DataOut/RenderProduct_Replicator/rgb/rgb_0000.png")
+    # Load numpy array with depth values and convert to depth image
+    depth_array = np.load("/home/gjfh119/Documents/DataOut/RenderProduct_Replicator/distance_to_camera/distance_to_camera_0000.npy")
+    print(depth_array.shape)
+    depth_img = o3d.geometry.Image(depth_array)
 
-# Load numpy array with depth values and convert to depth image
-depth_array = np.load("/home/gjfh119/Documents/DataOut/RenderProduct_Replicator/distance_to_camera/distance_to_camera_0000.npy")
-print(depth_array.shape)
-depth_img = o3d.geometry.Image(depth_array)
+    #Create RGBD image from color and depth images
+    rgbd_img = o3d.geometry.RGBDImage.create_from_color_and_depth(color_img, depth_img, convert_rgb_to_intensity=False)
 
-#Create RGBD image from color and depth images
-rgbd_img = o3d.geometry.RGBDImage.create_from_color_and_depth(color_img, depth_img, convert_rgb_to_intensity=False)
+    # Display RGBD image components
+    if display:
+        f, plot = plt.subplots(1,2)
+        plot[0].imshow(rgbd_img.color)
+        plot[1].imshow(rgbd_img.depth)
+        plt.show()
 
-# Display RGBD image components
-f, plot = plt.subplots(1,2)
-plot[0].imshow(rgbd_img.color)
-plot[1].imshow(rgbd_img.depth)
-plt.show()
+    return rgbd_img
