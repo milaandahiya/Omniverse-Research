70ec4e66ce409e8058d76c496506f20bdc350d04
Milaan Dahiya <milaandahiya@gmail.com>
2023-06-23 16:45:47 -0500

    doesn't work

--- tools/generatePLY.py
+++ tools/generatePLY.py
@@ -4,14 +4,22 @@ from generateRGBD import generateRGBD
 import numpy as np
 
 # Get the RGBD image generated from Omniverse data
-rgbd = generateRGBD(image=10, display=True)
+rgbd1 = generateRGBD(camera=0, image=10, display=True)
+rgbd2 = generateRGBD(camera=1, image=10, display=True)
 
 # Set camera intrinsic values
 intrinsic = o3d.camera.PinholeCameraIntrinsic(1280, 720, 1500, 1500, 640, 360)
 
 # Create point cloud, translate right side up, and display
-pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, intrinsic)
-print(pcd.has_colors())
-# pcd.colors = o3d.utility.Vector3dVector(np.array(rgbd.color))
+pcd1 = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd1, intrinsic)
+pcd2 = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd2, intrinsic)
+
+# Combine the two point clouds TODO fix orientation with camera extrinsics
+pcd = pcd1 + pcd2
 pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
 o3d.visualization.draw_geometries([pcd])
+
+# pcd1.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+# pcd2.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+# o3d.visualization.draw_geometries([pcd1])
+# o3d.visualization.draw_geometries([pcd2])
--- tools/generateRGBD.py
+++ tools/generateRGBD.py
@@ -2,12 +2,13 @@ import open3d as o3d
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generateRGBD(image: int, display: bool = False):
+def generateRGBD(camera: int, image: int, display: bool = False):
     # Get color image
-    color_img = o3d.io.read_image(f"/home/gjfh119/Documents/DataOut/RenderProduct_Replicator/rgb/rgb_{image:{0}{4}}.png")
+    camera_str = "" if camera == 0 else f"_{camera:{0}{2}}"
+    color_img = o3d.io.read_image(f"/home/gjfh119/Documents/DataOut/RenderProduct_Replicator{camera_str}/rgb/rgb_{image:{0}{4}}.png")
 
     # Load numpy array with depth values, scale (doesn't change pointcloud), and convert to depth image
-    depth_array = np.load(f"/home/gjfh119/Documents/DataOut/RenderProduct_Replicator/distance_to_camera/distance_to_camera_{image:{0}{4}}.npy")
+    depth_array = np.load(f"/home/gjfh119/Documents/DataOut/RenderProduct_Replicator{camera_str}/distance_to_camera/distance_to_camera_{image:{0}{4}}.npy")
     depth_array *= 50
     depth_img = o3d.geometry.Image(depth_array)
 
