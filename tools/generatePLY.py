7802db38c134bb1befc064082819882fe723ff1c
Milaan Dahiya <milaandahiya@gmail.com>
2023-06-29 11:32:06 -0500

    cleanup generatePLY

--- tools/generatePLY.py
+++ tools/generatePLY.py
@@ -4,8 +4,9 @@ from generateRGBD import generateRGBD
 import numpy as np
 
 # Get the RGBD image generated from Omniverse data
-rgbd1 = generateRGBD(camera=0, image=1)#, display=True)
-rgbd2 = generateRGBD(camera=1, image=1, display=True)
+displayAll = False
+rgbd1 = generateRGBD(camera=0, image=1, display=displayAll)
+rgbd2 = generateRGBD(camera=1, image=1, display=displayAll)
 
 # Set camera intrinsic values
 intrinsic = o3d.camera.PinholeCameraIntrinsic(1280, 720, 1500, 1500, 640, 360)
@@ -17,30 +18,13 @@ pcd2 = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd2, intrinsic)
 # Compute odometry between RGBD images (not strictly necessary for Omniverse cameras with known translations, but
 # probably is necessary for real world cameras)
 option = o3d.pipelines.odometry.OdometryOption() # TODO maybe set options
-odo_init = np.identity(4)
-[success_color_term, trans_color_term, info] = o3d.pipelines.odometry.compute_rgbd_odometry(rgbd1, rgbd2, intrinsic,
-                                        odo_init, o3d.pipelines.odometry.RGBDOdometryJacobianFromColorTerm(), option) # colorterm
-[success_hybrid_term, trans_hybrid_term, info] = o3d.pipelines.odometry.compute_rgbd_odometry(rgbd1, rgbd2, intrinsic,
-                                        odo_init, o3d.pipelines.odometry.RGBDOdometryJacobianFromHybridTerm(), option) # hybridterm
+success, trans, info = o3d.pipelines.odometry.compute_rgbd_odometry(rgbd1, rgbd2, intrinsic, option=option)
 
-if success_color_term:
-    print("RGBD Odometry")
-    source_pcd_color_term = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd1, intrinsic)
-    source_pcd_color_term.transform(trans_color_term)
-    o3d.visualization.draw_geometries([pcd2, source_pcd_color_term])
+if success:
+    print("Transformation success")
+    pcd1.transform(trans)
 
-if success_hybrid_term:
-    print("Hybrid RGBD Odometry")
-    source_pcd_hybrid_term = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd1, intrinsic)
-    source_pcd_hybrid_term.transform(trans_hybrid_term)
-    o3d.visualization.draw_geometries([pcd2, source_pcd_hybrid_term])
-
-
-# Translate right side up, and display
-# pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-# o3d.visualization.draw_geometries([pcd])
-
-# pcd1.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-# pcd2.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-# o3d.visualization.draw_geometries([pcd1])
-# o3d.visualization.draw_geometries([pcd2])
+# Translate right side up, and display right side up
+pcd = pcd1 + pcd2
+pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+o3d.visualization.draw_geometries([pcd])
