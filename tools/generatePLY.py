eb0f32b605bbfb0ae599e6a0ac448fd574c51b36
Milaan Dahiya <milaandahiya@gmail.com>
2023-07-07 14:48:13 -0500

    try to fix pointclouds

--- tools/generatePLY.py
+++ tools/generatePLY.py
@@ -1,5 +1,6 @@
 import open3d as o3d
 from generateRGBD import generateRGBD
+import math
 
 # Get the RGBD image generated from Omniverse data
 displayAll = False
@@ -15,12 +16,17 @@ pcd2 = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd2, intrinsic)
 
 # Compute odometry between RGBD images (not strictly necessary for Omniverse cameras with known translations, but
 # probably is necessary for real world cameras)
-option = o3d.pipelines.odometry.OdometryOption(depth_diff_max=0.2)
-success, trans, info = o3d.pipelines.odometry.compute_rgbd_odometry(rgbd1, rgbd2, intrinsic, option=option)
+# option = o3d.pipelines.odometry.OdometryOption(depth_diff_max=0.5)
+# success, trans, info = o3d.pipelines.odometry.compute_rgbd_odometry(rgbd1, rgbd2, intrinsic, option=option)
 
-if success:
-    print("Transformation success")
-    pcd1.transform(trans)
+# if success:
+#     print("Transformation success")
+#     pcd1.transform(trans)
+
+# rotate pcd2, attempt multiway registration
+angle = -35 * math.pi / 180
+pcd2.rotate([[math.cos(angle), 0, math.sin(angle)], [0, 1, 0], [-math.sin(angle), 0, math.cos(angle)]])
+pcd2.translate([-0.105, 0, -0.15])
 
 # Combine point clouds, and display right side up
 pcd = pcd1 + pcd2
